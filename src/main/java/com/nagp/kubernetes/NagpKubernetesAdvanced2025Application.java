9085c3f26a644997b1da7c05b66ec15c1c188abd
AzureAD\PrateekSharma <prateek.sharma3@osttra.com>
2025-08-03 13:32:44 +0530

    Initial commit

---
+++ src/main/java/com/nagp/kubernetes/NagpKubernetesAdvanced2025Application.java
@@ -0,0 +1,13 @@
+package com.nagp.kubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagpKubernetesAdvanced2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NagpKubernetesAdvanced2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nagp/kubernetes/NagpKubernetesAdvanced2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagp.kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagpKubernetesAdvanced2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
